bf5218746901645915c9b918aad2be21d5a0a070
Louisan Tchitoula <t.louisan@outlook.fr>
2025-02-11 15:56:02 +0100

    feat: frontend init, shadcn tailwind

---
+++ client/src/layout/home/home.page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function HomePage() {
+  return (
+    <div>
+        <h1>HomePage</h1>
+        <Button>Bouton qui clic</Button>
+    </div>
+  )
+}
