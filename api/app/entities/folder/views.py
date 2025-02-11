38938b9819800036335bc5c56c21eaf2ba12200b
lsun <lsun.github@gmail.com>
2025-02-11 21:06:48 +0100

    feat: add user association to Folder model and create FolderViewSet with authentication

---
+++ api/app/entities/folder/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets, permissions
+from app.entities.folder.models import Folder
+from app.entities.folder.serializer import FolderSerializer
+
+class FoderViewSet(viewsets.ModelViewSet):
+    queryset = Folder.objects.all()
+    serializer_class = FolderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(client=self.request.user.client)
