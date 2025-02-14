8839a4deebdb9210c8541edc7ac8075523c3bc0b
Feydai <lsun.github@gmail.com>
2025-02-14 14:15:05 +0100

    refacto folder -> contract

---
+++ api/app/entities/contract/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+from app.entities.contract.models import Contract
+from app.entities.contract.serializer import ConctractSerializer
+
+class ContractViewSet(viewsets.ModelViewSet):
+    queryset = Contract.objects.all()
+    serializer_class = ConctractSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        print(f"Utilisateur connecté : {user}")
+        return Contract.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        print("perform_create() appelée !")
+        print("Utilisateur authentifié :", self.request.user)
+        print("Type utilisateur :", type(self.request.user))
+
+        if self.request.user.is_anonymous:
+            raise PermissionDenied("Utilisateur non authentifié !")
+
+        serializer.save(user=self.request.user)
