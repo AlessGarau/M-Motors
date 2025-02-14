a538a48dbddd8564ce4918f59a3face32795204e
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-02-14 10:37:25 +0100

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if test -d ./api/venv; then 
-   cd ./api && source venv/bin/activate && pip install -r requirements.txt;
-else cd ./api && python3 -m venv venv  && source venv/bin/activate && pip install -r requirements.txt;
+   cd ./api && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt && python3 manage.py migrate;
+else cd ./api && python3 -m venv venv  && source venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt && python3 manage.py migrate;
 fi
 cd ../client && npm install 
 cd ../ && docker-compose -f Docker/docker-compose.dev.yml -p motor-m up --build --no-start
