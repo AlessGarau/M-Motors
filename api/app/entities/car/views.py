0fcd9a335da37f0d1121872db512bd35ce199f24
Maria Petersen <maria.petersen@ealico.com>
2025-02-17 16:08:18 +0100

    ADD permissions for car

--- api/app/entities/car/views.py
+++ api/app/entities/car/views.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from app.entities.car.models import Car
 from app.entities.car.serializer import CarSerializer
 from app.entities.car.filters import CarFilter 
 from rest_framework.pagination import PageNumberPagination
+from app.permissions.admin_permissions import IsAdminProfile
 
 class CarPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
@@ -15,3 +16,10 @@ class CarViewSet(viewsets.ModelViewSet):
     pagination_class = CarPagination
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = CarFilter
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']: 
+            return [permissions.AllowAny()]
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminProfile()] 
+        return super().get_permissions()
