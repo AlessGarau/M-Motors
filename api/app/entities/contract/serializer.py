1439d7a176ac835c61ae3cc4ee8b1faa0e442be4
LeBenjos <benjamin.schinkel01@outlook.fr>
2025-02-17 16:41:45 +0100

    fix: dupplicate pdf and optionnal userfield

--- api/app/entities/contract/serializer.py
+++ api/app/entities/contract/serializer.py
@@ -1,9 +1,3 @@
-from rest_framework import serializers
-from django.core.files import File
-import os
-from app.entities.contract.models import Contract
-from app.entities.contract.utils import generate_contract_pdf
-
 import os
 from django.core.files import File
 from rest_framework import serializers
@@ -14,9 +8,15 @@ class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         fields = '__all__'
+        extra_kwargs = {'user': {'required': False}}
 
     def create(self, validated_data):
-        contract = Contract.objects.create(**validated_data)
+        request = self.context.get('request')
+
+        if request and hasattr(request, "user"):
+            validated_data['user'] = request.user
+
+        contract = super().create(validated_data)
         pdf_path = generate_contract_pdf(contract)
 
         if not os.path.exists(pdf_path):
@@ -24,10 +24,10 @@ class ContractSerializer(serializers.ModelSerializer):
 
         try:
             with open(pdf_path, "rb") as pdf_file:
-                contract.pdf_file.save(f"contract_{contract.id}.pdf", File(pdf_file))
+                contract.pdffile.save(f"contract{contract.id}.pdf", File(pdf_file))
                 contract.save()
-        except Exception as e:
-            print(f"{e}")
+        except Exception:
+            pass
 
         os.remove(pdf_path)
         return contract
