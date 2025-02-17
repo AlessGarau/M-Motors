a43ed87094de65ed52de6a11e640c8b4a6096702
Louisan Tchitoula <t.louisan@outlook.fr>
2025-02-17 10:33:17 +0100

    fix: brands to string without choices check constraint

--- api/app/entities/car/models.py
+++ api/app/entities/car/models.py
@@ -1,30 +1,19 @@
 from typing import Required
 from django.db import models
 
+
 class Car(models.Model):
-    BRAND_CHOICES = {
-        "RENAULT": "Renault",
-        "PEUGEOT": "Peugeot",
-        "FIAT": "Fiat",
-        "BMW": "BMW",
-        "AUDI": "Audi",
-        "NISSAN": "Nissan",
-        "CITROEN": "Citroën",
-    }
     SERVICE_TYPE_CHOICES = {
         "SALE": "Vente",
         "RENTAL": "Location"
     }
-    brand = models.CharField(
-        max_length=15,
-        choices=BRAND_CHOICES,
-    )
     service_type = models.CharField(
         choices=SERVICE_TYPE_CHOICES,
         max_length=10,
         default="Vente"
     )
-    model = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
     year = models.IntegerField()
     kilometers = models.BigIntegerField()
     price = models.IntegerField()
