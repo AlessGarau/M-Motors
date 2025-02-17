510dcc93924540c29dac7e8361a7ad70ab76181d
Louisan Tchitoula <t.louisan@outlook.fr>
2025-02-17 11:14:48 +0100

    fix: db docker env

--- api/moters/settings.py
+++ api/moters/settings.py
@@ -110,8 +110,12 @@ WSGI_APPLICATION = "moters.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3", 
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "admin",
+        "USER": "admin",
+        "PASSWORD": "root",
+        "HOST": "motor-m-db-db",
+        "PORT": "5432",
     }
 }
 
