598fbb6693dedadc9fa0471c1949d5247bfb7ddb
Louisan Tchitoula <t.louisan@outlook.fr>
2025-02-19 15:28:28 +0100

    chore: unused imports

--- client/src/pages/admin/panels/contracts/ContractsForm.tsx
+++ client/src/pages/admin/panels/contracts/ContractsForm.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ContractsForm() {
   return (
     <div>ContractsForm</div>
--- client/src/pages/admin/panels/contracts/ContractsPanel.tsx
+++ client/src/pages/admin/panels/contracts/ContractsPanel.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ContractsPanel() {
   return (
     <div>ContractsPanel</div>
