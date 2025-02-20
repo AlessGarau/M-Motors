3a34fa6e68196d309a50b70c40e5760021ff2405
Feydai <lsun.github@gmail.com>
2025-02-20 20:21:07 +0100

    fix Correct contract status update via PATCH on fronted

--- client/src/lib/queries.ts
+++ client/src/lib/queries.ts
@@ -1,11 +1,19 @@
-export async function fetchWithAuth(url: string, options = {}) {
+export async function fetchWithAuth(url: string, options: RequestInit = {}) {
     const token = localStorage.getItem("token");
+    const headers = new Headers(options.headers || {});
+
+    // Assurez-vous de ne pas écraser d'autres en-têtes existants
+    if (token) {
+        headers.set("Authorization", `Token ${token}`);
+    }
+
+    // Définit le Content-Type pour les requêtes JSON si non défini
+    if (!headers.has("Content-Type") && options.method !== "GET") {
+        headers.set("Content-Type", "application/json");
+    }
     const response = await fetch(url, {
         ...options,
-        headers: {
-           "Authorization": `Token ${token}`
-        },
+        headers,
     });
-
     return response;
 }
--- client/src/pages/admin/panels/contracts/ContractsPanel.tsx
+++ client/src/pages/admin/panels/contracts/ContractsPanel.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
     useReactTable,
     getCoreRowModel,
@@ -7,11 +7,19 @@ import {
     PaginationState,
 } from "@tanstack/react-table";
 import { List } from "../../common/list";
-import { ContractListResponse } from "./types";
+import { Contract, ContractListResponse } from "./types";
 import { columns } from "./ContractsColumn";
 import { DataTablePagination } from "../../common/pagination";
 import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { fetchWithAuth } from "@/lib/queries";
 
 const fetchContractData = async (
@@ -29,6 +37,33 @@ const fetchContractData = async (
     if (!response.ok) {
         throw new Error("Failed to fetch contracts data");
     }
+    const data = await response.json();
+    console.log("Données récupérées depuis l'API :", data);
+    return data;
+};
+
+const updateContractStatus = async (id: number, status: string) => {
+    const url = `${import.meta.env.VITE_API_URL}contract/${id}/?admin=true`;
+
+    const response = await fetchWithAuth(url, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status }),
+    });
+
+    if (!response.ok) {
+        console.error(
+            "Failed to update contract status. Status:",
+            response.status
+        );
+        const errorMessage = await response.text();
+        console.error("Response:", errorMessage);
+        throw new Error(
+            `Failed to update contract status. Status: ${response.status}`
+        );
+    }
     return response.json();
 };
 
@@ -38,13 +73,33 @@ function ContractsPanel() {
         pageSize: 10,
     });
 
-    const navigate = useNavigate();
-
     const { pageIndex, pageSize } = pagination;
+    const queryClient = useQueryClient();
 
     const { data, error, isLoading, refetch } = useQuery<ContractListResponse>({
         queryKey: ["contracts", pageIndex, pageSize],
         queryFn: () => fetchContractData(pageIndex, pageSize),
+    });
+
+    const mutation = useMutation({
+        mutationFn: (variables: { id: number; status: string }) =>
+            updateContractStatus(variables.id, variables.status),
+        onSuccess: (updatedContract: Contract) => {
+            queryClient.setQueryData<ContractListResponse>(
+                ["contracts", pageIndex, pageSize],
+                (oldData) => {
+                    if (!oldData) return oldData;
+
+                    const updatedResults = oldData.results.map((contract) =>
+                        contract.id === updatedContract.id
+                            ? { ...contract, status: updatedContract.status }
+                            : contract
+                    );
+
+                    return { ...oldData, results: updatedResults };
+                }
+            );
+        },
     });
 
     const table = useReactTable({
@@ -55,16 +110,28 @@ function ContractsPanel() {
                 id: "actions",
                 header: "Actions",
                 cell: ({ row }) => (
-                    <div className="space-x-2">
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() =>
-                                row.original.id && handleUpdate(row.original.id)
+                    <div className="flex items-center gap-2">
+                        <Select
+                            onValueChange={(value) =>
+                                row.original.id &&
+                                mutation.mutate({
+                                    id: row.original.id,
+                                    status: value,
+                                })
                             }
+                            value={row.original.status}
                         >
-                            Update Status
-                        </Button>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Update Status" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="PENDING">Pending</SelectItem>
+                                <SelectItem value="APPROVE">Approve</SelectItem>
+                                <SelectItem value="REJECTED">
+                                    Rejected
+                                </SelectItem>
+                            </SelectContent>
+                        </Select>
                         <Button
                             variant="destructive"
                             size="sm"
@@ -89,7 +156,7 @@ function ContractsPanel() {
     const handleDelete = async (id: number) => {
         try {
             await fetchWithAuth(
-                `${import.meta.env.VITE_API_URL}contract/${id}/`,
+                `${import.meta.env.VITE_API_URL}contract/${id}/?admin=true`,
                 {
                     method: "DELETE",
                     credentials: "include",
@@ -101,40 +168,44 @@ function ContractsPanel() {
         }
     };
 
-    const handleUpdate = (id: number) => {
-        console.log("Navigating to update form for contract ID:", id);
-        navigate(`/admin/contracts/update/${id}?admin=true`);
-    };
-
     return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Contracts Management</h1>
+        <Card className="p-6">
+            <CardHeader>
+                <CardTitle className="text-2xl font-bold">
+                    Contracts Management
+                </CardTitle>
+            </CardHeader>
+            <Separator className="my-4" />
+            <CardContent>
+                {isLoading && (
+                    <div className="flex justify-center items-center">
+                        <span>Loading contracts...</span>
+                    </div>
+                )}
 
-            {isLoading && (
-                <div className="flex justify-center items-center">
-                    <span>Loading contracts...</span>
-                </div>
-            )}
+                {error && (
+                    <div className="text-red-600">
+                        <p>Error: {error.message}</p>
+                        <Button
+                            variant="outline"
+                            onClick={() => window.location.reload()}
+                        >
+                            Retry
+                        </Button>
+                    </div>
+                )}
 
-            {error && (
-                <div className="text-red-600">
-                    <p>Error: {error.message}</p>
-                    <button onClick={() => window.location.reload()}>
-                        Retry
-                    </button>
-                </div>
-            )}
-
-            {!isLoading && !error && data && (
-                <>
-                    <List table={table} />
-                    <DataTablePagination
-                        table={table}
-                        setPagination={setPagination}
-                    />
-                </>
-            )}
-        </div>
+                {!isLoading && !error && data && (
+                    <>
+                        <List table={table} />
+                        <DataTablePagination
+                            table={table}
+                            setPagination={setPagination}
+                        />
+                    </>
+                )}
+            </CardContent>
+        </Card>
     );
 }
 
